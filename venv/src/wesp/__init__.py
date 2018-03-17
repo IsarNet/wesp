44d4b29e1687624e96c453c393a651b9b19c9f8c
Marcel Rummens <rummens.marcel@googlemail.com>
2018-03-17 15:26:03 +0100

    Test Commit

--- venv/src/wesp/__init__.py
+++ venv/src/wesp/__init__.py
@@ -6,3 +6,5 @@ if __name__ == '__main__':
     session = Session(hostname='192.168.178.240', community='MWb2JVBn', version=2)
     Snmp(session)
     Snmp.print_walk('.1.3.6.1.4.1.14179.2.1.6.1.1')
+
+    #GitTest
