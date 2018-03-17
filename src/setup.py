eab5ce55de3649f82b2e17db438a0fc4abc9ffee
Marcel Rummens <rummens.marcel@googlemail.com>
2018-03-17 15:58:39 +0100

    Reorganized file structure

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='wesp',
+    version='',
+    packages=['wesp'],
+    package_dir={'': 'venv/src'},
+    url='',
+    license='',
+    author='marcel',
+    author_email='',
+    description=''
+)
