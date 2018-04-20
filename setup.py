eb1c98e022ecff44c9498ecf63db00726d65e25c
Marcel Rummens <rummens.marcel@googlemail.com>
2018-04-20 17:27:16 +0200

    Changed folder structure (removed src folder) Added .gitignore file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='wesp',
+    version='0.1',
+    packages=['wesp'],
+    url='',
+    license='0.1',
+    author='marcel',
+    author_email='',
+    description=''
+)
--- src/wesp/oids.py
+++
@@ -1,3 +0,0 @@
-#class which contains all OIDs for the attributes.
-class Oids():
-    snr = "1.11.1.1.1."
