49ff2a2358c34ebe19680fdeb30d86345fd1e0ef
Marcel Rummens <rummens.marcel@googlemail.com>
2018-05-22 15:41:07 +0200

    Added debian support to bash script

--- install_ with_dependencies.sh
+++ install_ with_dependencies.sh
@@ -4,13 +4,26 @@ os=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
 
 echo $os
 
-if [[ "$os" == '"Ubuntu"' ]]
+if [[ "$os" == *"Ubuntu"* ]]
 then
     echo "Will install for Ubuntu..."
     # install dependencies
     sudo apt-get install libsnmp-dev snmp-mibs-downloader gcc python-dev git python-setuptools
     echo "Dependencies installed"
     
+    #run python installer
+    sudo src/python setup.py install
+    echo "Installed wesp successfully"
+
+fi
+
+if [[ "$os" == *"Debian"* ]]
+then
+    echo "Will install for Debian..."
+    # install dependencies
+    sudo apt-get install libsnmp-dev snmp-mibs-downloader gcc python-dev git python-setuptools
+    echo "Dependencies installed"
+
     #run python installer
     sudo src/python setup.py install
     echo "Installed wesp successfully"
