d5d7df757fcd0c5ac4e06eb03d6ff6045d0bba88
Marcel Rummens <rummens.marcel@googlemail.com>
2018-05-25 19:20:52 +0200

    added man support to bash script

--- install_with_dependencies.sh
+++ install_with_dependencies.sh
@@ -3,8 +3,7 @@
 os=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo $os
-echo $DIR
+echo "Found OS $os, starting directory : $DIR"
 
 if [[ "$os" == *"Ubuntu"* ]]
 then
@@ -20,6 +19,9 @@ then
     
     #run python installer (installer needs to be run from inside the src folder)
     cd $DIR/src
+    echo "Creating man pages..."
+    sudo python setup.py --command-packages=click_man.commands man_pages --target /usr/share/man/man1/
+
     sudo python setup.py install
     echo "Installation completed. Check for errors!"
 
@@ -40,6 +42,9 @@ then
 
     #run python installer (installer needs to be run from inside the src folder)
     cd $DIR/src
+    echo "Creating man pages..."
+    sudo python setup.py --command-packages=click_man.commands man_pages --target /usr/share/man/man1/
+
     sudo python setup.py install
     echo "Installation completed. Check for errors!"
 
@@ -61,6 +66,10 @@ then
     #run python installer (installer needs to be run from inside the src folder)
     cd $DIR/src
     sudo python setup.py install
+
+    echo "Creating man pages..."
+    sudo python setup.py --command-packages=click_man.commands man_pages --target /usr/share/man/man1/
+
     echo "Installation completed. Check for errors!"
 
 fi
