29b9f0d64b7239a37fbcb96172155744a1d859ba
Marcel Rummens <marcel.rummens@airbus.com>
2018-07-12 12:46:08 +0200

    added feature to create column which are not present in the table. Fixed timezone bug with mySQL above version 5.5

--- README.md
+++ README.md
@@ -27,6 +27,8 @@ brew install git
 
 If your OS doesn’t ship with Net-SNMP 5.7.x, please follow instructions provided on the [Net-SNMP install page](%28http://www.net-snmp.org/docs/INSTALL.html) to build and install Net-SNMP 5.7.x on your system.
 
+Please note: To use the database function of this program, install a mySQL instance and make sure the machine 
+your using WESP on and the database share the same Timezone. 
 <br />
 <br />
 
--- src/wesp/database.py
+++ src/wesp/database.py
@@ -47,6 +47,28 @@ class Database:
     :meth:`wesp.helper.generate_parameter_insert_statement` function but has to be ran by the caller of 
     the :meth:`init_database` function."""
 
+    column_total_check = """SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA like '%%DATABASE%%' 
+    AND TABLE_NAME like '%%TABLE%%' AND COLUMN_NAME like '%%COLUMN%%' AND COLUMN_TYPE like '%%DATA_TYPE%%'"""
+    """
+    This statement is used to check if a column exists. Here the name and the data type is checked. 
+    """
+
+    column_only_name_check = """SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA like '%%DATABASE%%' 
+        AND TABLE_NAME like '%%TABLE%%' AND COLUMN_NAME like '%%COLUMN%%'"""
+    """
+    This statement is used to check if a column exists. Here only the name is checked. 
+    """
+
+    create_new_column = "ALTER TABLE %%DATABASE%%.%%TABLE%% ADD COLUMN `%%COLUMN%%` %%DATA_TYPE%%; "
+    """
+    This statement will created a new column in a existing table
+    """
+
+    modify_column = "ALTER TABLE %%DATABASE%%.%%TABLE%% MODIFY COLUMN `%%COLUMN%%` %%DATA_TYPE%%; "
+    """
+    This statement will modify the data type of an column to match the one registered in this program
+    """
+
     table_name = ""
     db_name = ""
     global_config = None
@@ -90,12 +112,12 @@ class Database:
         # add the database config to the class
         Database.global_config = config
 
-        Database.create_database_and_table_if_not_existing(config)
+        Database._create_database_and_table_if_not_existing(config)
 
         Database.ready = True
 
     @staticmethod
-    def create_database_and_table_if_not_existing(config):
+    def _create_database_and_table_if_not_existing(config):
 
         """
         Will check the existence of the database and table with the names specified in the :meth:`init_database`
@@ -126,17 +148,60 @@ class Database:
                     "Unknown MySQL Error at initial connection: `%s`" % (
                         err))
         else:
-            cur = cnx.cursor()
+            cur = cnx.cursor(buffered=True)
             # Create database, if not existing
             cur.execute(Database.databaseCreateStatement)
 
             # Create table if not existing
             cur.execute(Database.tableCreateStatement)
+
+            # Make sure all columns exist and have the right data type
+            Database._check_if_columns_exist(cur)
+
+            # Make sure data is committed to the database
+            cnx.commit()
         finally:
             if cur:
                 cur.close()
             if cnx:
                 cnx.close()
+
+    @staticmethod
+    def _check_if_columns_exist(cur):
+
+        # add database and table name to SQL statements
+        Database.column_only_name_check = Database.column_only_name_check.replace('%%DATABASE%%', Database.db_name).replace('%%TABLE%%', Database.table_name)
+        Database.column_total_check = Database.column_total_check.replace('%%DATABASE%%', Database.db_name).replace('%%TABLE%%', Database.table_name)
+        Database.create_new_column = Database.create_new_column.replace('%%DATABASE%%', Database.db_name).replace('%%TABLE%%', Database.table_name)
+        Database.modify_column = Database.modify_column.replace('%%DATABASE%%', Database.db_name).replace('%%TABLE%%', Database.table_name)
+
+        # for all parameter which have a data type check if the corresponding column exist
+        for parameter in AllParameter.get_all_parameter():
+            if parameter.db_data_type is not None:
+
+                # check if column with right name and data type exists
+                query = Database.column_total_check.replace('%%COLUMN%%', parameter.name).replace('%%DATA_TYPE%%', parameter.db_data_type)
+                cur.execute(query)
+
+                # rowcount of zero means no columns with this name and data type exists
+                if cur.rowcount == 0:
+
+                    # check if column only with right name exists, if so modify data type
+                    query = Database.column_only_name_check.replace('%%COLUMN%%', parameter.name)
+                    cur.execute(query)
+
+                    # rowcount of zero means no columns with this name exists
+                    # above zero means that a column has been found with the name
+                    if cur.rowcount == 0:
+
+                        # column has to be created
+                        query = Database.create_new_column.replace('%%COLUMN%%', parameter.name).replace('%%DATA_TYPE%%', parameter.db_data_type)
+                        cur.execute(query)
+
+                    else:
+                        # column has to be modified
+                        query = Database.modify_column.replace('%%COLUMN%%', parameter.name).replace('%%DATA_TYPE%%', parameter.db_data_type)
+                        cur.execute(query)
 
     @staticmethod
     def insert_data_set(data_set, ctx, time):
@@ -151,11 +216,11 @@ class Database:
 
         # Temporary add client_ip, client_mac and timestamp(which should not be represented in client_data
         # to prevent output to CLI)
-        # convert timestamp to ISO format to ensure that timezone is included and understand by DB
+        # convert timestamp to a format that is understood by the DB
         data_set = data_set.copy()
         data_set.update({'client_ip': ctx.obj['client_ip'],
                          'client_mac': ctx.obj['client_mac'],
-                         'timestamp': str(time.isoformat())})
+                         'timestamp': time.strftime('%Y-%m-%d %H:%M:%S')})
 
         cnx = cur = None
         try:
